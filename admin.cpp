4720c64dfc17a101c341a37ac2a7f1b1d09a0324
Matt Jang <48605082+mattjang1101@users.noreply.github.com>
2020-03-21 19:09:37 -0700

    updating admin

--- admin.cpp
+++ admin.cpp
@@ -9,6 +9,13 @@ Admin::Admin(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->stackedWidget->setCurrentWidget(ui->AdminMenu);
+    this->setWindowTitle("Maintenance");
+    ui->tabWidget->setTabText(0, "Campuses");
+    ui->tabWidget->setTabText(1, "Souvenirs");
+    ui->tabWidget_2->setTabText(0, "Add/Delete");
+    ui->tabWidget_2->setTabText(1, "Edit");
+
+
 }
 
 Admin::~Admin()
@@ -16,3 +23,64 @@ Admin::~Admin()
     delete ui;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Add button
+void Admin::on_AddButton_clicked()
+{
+    /*
+    ui->AdminTable->setModel(databaseObj.loadNewCampusInfoIntoCollegeDistances());
+    ui->AdminTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    */
+    databaseObj.loadNewCampusInfoIntoCollegeDistances();
+
+}
+
+// Delete button
+void Admin::on_revertButton_clicked()
+{
+    /*
+    ui->AdminTable->setModel(databaseObj.deleteNewCampusInfoIntoCollegeDistances());
+    ui->AdminTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    */
+    databaseObj.deleteNewCampusInfoIntoCollegeDistances();
+}
+
+
+// Change prices souvenir
+void Admin::on_ChangePricesButton_clicked()
+{
+    //databaseObj.changeCampusSouvenirPrices();
+
+
+}
+
+
+// Add New Souvenirs
+void Admin::on_pushButton_clicked()
+{
+
+}
+
+
+
+// Delete Souvenirs
+void Admin::on_pushButton_2_clicked()
+{
+
+}
